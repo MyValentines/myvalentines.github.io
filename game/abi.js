4fd2f0cd735e03f89a62dd729cb661d0abbe918c
MyValentines <104030850+MyValentines@users.noreply.github.com>
2022-06-17 21:32:15 +0100

    Delete abi.js

--- game/abi.js
+++
@@ -1,1 +0,0 @@
-const address="0x731d6C6eF47e62ae0e61f51fE21EC5a513d75C68";const abi=[{inputs:[{internalType:"address",name:"_owner",type:"address"}],name:"walletOfOwner",outputs:[{internalType:"uint256[]",name:"",type:"uint256[]"}],stateMutability:"view",type:"function"}];
